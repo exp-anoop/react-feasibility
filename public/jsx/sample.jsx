2cdf6775c98bba2713bb8fe07ec318c45cb4120c
Anoop P R <anoop.pr@experionglobal.com>
2017-01-27 17:56:16 +0530

    Understanding states and props

--- public/jsx/sample.jsx
+++ public/jsx/sample.jsx
@@ -7,9 +7,24 @@ var Product = React.createClass({
         };
     },
 
-    addItem: function() {
-        console.log('Name', this.props.name);
-        console.log('Price', this.props.price);
+    getInitialState: function() {
+        return {
+            quantity: 0,
+            total:0
+        }
+    },
+
+    addQuantity: function(e) {
+        e.preventDefault();
+        var quantityRef = this.refs.quantity;
+        var quantity = parseInt(quantityRef.value);
+        if(quantity > 0) {
+            this.setState({
+                quantity: quantityRef.value,
+                total: quantity * this.props.price
+            });
+        }
+        quantityRef.value = 0;
     },
 
     render: function() {
@@ -17,7 +32,11 @@ var Product = React.createClass({
             <div>
                 <h2>Name: {this.props.name}</h2>
                 <h3>Price: ${this.props.price}</h3>
-                <button onClick={this.addItem}>Add</button>
+                <h3>Total: {this.state.quantity} x ${this.props.price} = ${this.state.total}</h3>
+                <form onSubmit={this.addQuantity}>
+                    <input type="number" ref='quantity' />
+                    <button type="submit">Add Quantity</button>
+                </form>
                 <hr></hr>
             </div>
         );
