772a7a27e293414279b118449e097c252af06e17
Anoop P R <anoop.pr@experionglobal.com>
2017-01-27 18:47:37 +0530

    Nested components and data sharing

--- public/jsx/sample.jsx
+++ public/jsx/sample.jsx
@@ -1,49 +1,90 @@
-var Product = React.createClass({
-
+var ProductView = React.createClass({
     getDefaultProps: function() {
         return {
             name: "No Title",
-            price: 0
-        };
-    },
-
-    getInitialState: function() {
-        return {
-            quantity: 0,
-            total:0
+            price: 0,
+            total: 0
         }
-    },
-
-    addQuantity: function(e) {
-        e.preventDefault();
-        var quantityRef = this.refs.quantity;
-        var quantity = parseInt(quantityRef.value);
-        if(quantity > 0) {
-            this.setState({
-                quantity: quantityRef.value,
-                total: quantity * this.props.price
-            });
-        }
-        quantityRef.value = 0;
     },
 
     render: function() {
         return (
             <div>
                 <h2>Name: {this.props.name}</h2>
-                <h3>Price: ${this.props.price}</h3>
-                <h3>Total: {this.state.quantity} x ${this.props.price} = ${this.state.total}</h3>
-                <form onSubmit={this.addQuantity}>
+                <h4>Price: ${this.props.price}</h4>
+                <h3>Total: ${this.props.total}</h3>
+            </div>
+        );
+    }
+});
+
+var QuantityForm = React.createClass({
+
+    returnQuantity: function(e) {
+        e.preventDefault();
+        var quantityRef = this.refs.quantity;
+        var quantity = parseInt(quantityRef.value);
+        // quantityRef.value = 0;
+        this.props.onQuantityUpdate(quantity);
+    },
+
+    render: function() {
+        return (
+            <div>
+                <form onSubmit={this.returnQuantity}>
                     <input type="number" ref='quantity' />
                     <button type="submit">Add Quantity</button>
                 </form>
+            </div>
+        );
+    }
+})
+
+var ProductItem = React.createClass({
+
+    getDefaultProps: function() {
+        return {
+            name: "No Title",
+            price: 0
+        }
+    },
+
+    getInitialState : function() {
+        return {
+            total: 0
+        }
+    },
+
+    handleOnQuantityUpdate: function(quantity) {
+        this.setState({
+            total : quantity * this.props.price
+        });
+    },
+
+    render: function() {
+        return (
+            <div>
+                <ProductView name={this.props.name} price={this.props.price} total={this.state.total}/>
+                <QuantityForm onQuantityUpdate={this.handleOnQuantityUpdate} />
                 <hr></hr>
             </div>
         );
     }
 });
 
+var ProductList = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <ProductItem name="Apple" price={200}/>
+                <ProductItem name="Samsung" price={140}/>
+                <ProductItem name="Nokia" price={130}/>
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <Product name="Apple" price={200}/>,
+    <ProductList />,
     document.getElementById('root')
 );
