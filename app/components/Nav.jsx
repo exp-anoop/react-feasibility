4fcec8a941bcaf54335c352fbb4f4842eb2b4aef
Anoop P R <anoop.pr@experionglobal.com>
2017-02-10 20:09:02 +0530

    Nav styling

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,9 +4,20 @@ import {Link, IndexLink} from 'react-router';
 var Nav = (props) => {
     return(
         <div>
-            <IndexLink to="/" activeClassName="active">Home</IndexLink>
-            <Link to="/about" activeClassName="active">About</Link>
-            <Link to="/products" activeClassName="active">Products</Link>
+            <div className="top-bar">
+                <div className="top-bar-title">
+                    <strong>React</strong>
+                </div>
+                <div id="responsive-menu">
+                    <div className="top-bar-left">
+                        <ul className="menu">
+                            <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Home</IndexLink></li>
+                            <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+                            <li><Link to="/products" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Products</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
